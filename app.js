6e8e4f4de05ccfbe2dd2bbd9ba1c6360d561b7b7
Sean Davis <daleran@gmail.com>
2019-06-22 15:56:49 +0900

    Added express.js, env, and app folders

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("It worked");
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Listening on port ${port}.`));
+
