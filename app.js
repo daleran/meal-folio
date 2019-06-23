758507a9861d6c844ac90328d3a62bfb6fbea076
Sean Davis <daleran@gmail.com>
2019-06-23 15:08:13 +0900

    AppJS responds to POST with JSON echo

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })) 
 app.use(express.static('public'));
+
+var handleRecipe = function(req, res){
+  var newRecipe = {
+    recipeName: req.body.recipeName,
+    ingredients: req.body.ingredients,
+    directions: req.body.directions
+  };
+  res.send(JSON.stringify(newRecipe));
+}
+
 app.get('/', (req, res) => res.sendFile('/public/index.html'));
-app.post('/recipes', (req, res) => res.send('Form recieved.'));
+app.post('/recipes', handleRecipe);
 
 app.listen(port, () => console.log(`Listening on port ${port}.`));
 
