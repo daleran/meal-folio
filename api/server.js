07b7449efdf557c574e4ecfda0d9f5cce442f093
daleran <daleran@gmail.com>
2019-06-25 12:30:54 +0900

    API-created basic serever

--- api/server.js
+++ api/server.js
@@ -1,3 +1,8 @@
+require('dotenv').config();
+
 var express = require('express');
 var app = express();
+
+
+app.listen(process.env.PORT, ()=> console.log("Serving meal-folio API on port: "+process.env.PORT));
 
