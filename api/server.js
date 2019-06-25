daa8037306a5fe0bdfafdceb33d45fb55957ed1c
daleran <daleran@gmail.com>
2019-06-25 15:01:46 +0900

    Added root get

--- api/server.js
+++ api/server.js
@@ -4,5 +4,7 @@ var express = require('express');
 var app = express();
 
 
-app.listen(process.env.PORT, ()=> console.log("Serving meal-folio API on port: "+process.env.PORT));
+app.get('/',(req,res)=>  res.send("API root page"));
 
+
+app.listen(process.env.PORT, ()=> console.log("Serving meal-folio API on port: "+process.env.PORT));
