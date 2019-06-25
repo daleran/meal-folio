59bab7196d14aac22412bd41af8e20713db37e6d
daleran <daleran@gmail.com>
2019-06-25 16:00:34 +0900

    Added basic recipe routes with debug msgs

--- api/server.js
+++ api/server.js
@@ -6,5 +6,9 @@ var app = express();
 
 app.get('/',(req,res)=>  res.send("API root page"));
 
+app.use('/recipes',require('./recipes/routes'));
+
+app.all('*', (req,res)=> res.status(404).send('404 Not Found'));
+
 
 app.listen(process.env.PORT, ()=> console.log("Serving meal-folio API on port: "+process.env.PORT));
