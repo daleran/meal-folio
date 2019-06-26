4944094441f1061af6dda135b1fccb156139fbce
daleran <daleran@gmail.com>
2019-06-26 14:30:10 +0900

    Split server into server and api

--- api/src/server.js
+++ api/src/server.js
@@ -1,14 +1,7 @@
 require('dotenv').config();
-var express = require('express');
-var api = express();
+const api = require('./api');
+const port = process.env.PORT;
 
-require('./db/mongoose');
-
-
-
-api.get('/',(req,res)=>  res.send("API root page"));
-api.use(express.json());
-api.use('/recipes',require('./recipes/recipe.routes'));
-api.all('*', (req,res)=> res.status(404).send('404 Not Found'));
-
-api.listen(process.env.PORT, ()=> console.log("Serving meal-folio API on port: "+process.env.PORT));
+api.listen(port, ()=> {
+    console.log("Serving meal-folio API on port: "+port)
+});
