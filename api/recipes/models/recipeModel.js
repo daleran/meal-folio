2c635f1a9086f6672720fbc5ca89caa0ee392180
daleran <daleran@gmail.com>
2019-06-26 10:19:01 +0900

    Added recipe and ingredient schema

--- api/recipes/models/recipeModel.js
+++ api/recipes/models/recipeModel.js
@@ -2,8 +2,34 @@ module.exports = (function(){
 
     const mongoose = require('mongoose');
     
+    var ingredientSchema = new mongoose.Schema({
+        amount: {
+            type: Number,
+        },
+        unit: {
+            type: String,
+        },
+        ingredient: {
+            type: String
+        }
+    });
+    
     var recipeSchema = new mongoose.Schema({
-        name: String
+        name: {
+            type: String, 
+            trim: true, 
+            required: "Recipe Name is Required"
+        },
+        author: {
+            type: String,
+            trim: true
+        },
+        tags: [{type: String, trim: true}],
+        ingredients: [{
+            type: ingredientSchema
+        }],
+        steps: [String],
+        notes: [String]
     });
 
     const model = mongoose.model("Recipe",recipeSchema);
