22a2d750002649a365ae191cdd488c9e4f615269
daleran <daleran@gmail.com>
2019-06-26 13:58:32 +0900

    Restructured files

--- api/src/recipes/recipe.routes.js
+++ api/src/recipes/recipe.routes.js
@@ -1,13 +1,11 @@
 var recipesEndpoint = require('express').Router();
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
-var recipes = require('../models/recipeModel');
+var recipes = require('./recipe.model');
 
 recipesEndpoint.get('/', function(req,res){
     res.status(200).send('Get All Recipes');
 });
 
-recipesEndpoint.post('/', jsonParser, function(req,res){
+recipesEndpoint.post('/', function(req,res){
     
     res.status(201).send('Post a new recipe');
 });
@@ -16,7 +14,7 @@ recipesEndpoint.get('/:id', function(req,res){
     res.status(200).send('Get a recipe with the id '+req.params.id);
 });
 
-recipesEndpoint.put('/:id', jsonParser, function(req,res){
+recipesEndpoint.put('/:id', function(req,res){
     res.status(200).send('Update a recipe with the id '+req.params.id);
 });
 
--- api/src/server.js
+++ api/src/server.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 var express = require('express');
-var bodyParser = require('body-parser');
-var db = require('./db');
 var api = express();
 
+require('./db/mongoose');
+
+
+
 api.get('/',(req,res)=>  res.send("API root page"));
-api.use('/recipes',require('./recipes/routes'));
+api.use(express.json());
+api.use('/recipes',require('./recipes/recipe.routes'));
 api.all('*', (req,res)=> res.status(404).send('404 Not Found'));
 
 api.listen(process.env.PORT, ()=> console.log("Serving meal-folio API on port: "+process.env.PORT));
