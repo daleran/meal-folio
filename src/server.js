7978c7b080824055f20ae6cf3b1150ed08cbf05d
daleran <daleran@gmail.com>
2019-06-28 12:55:20 +0900

    Fixed double port listen issue

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const api = require('./api')
 const port = process.env.PORT || 3000
 
 api.listen(port, () => {
-  console.log(`Serving meal-folio API on port: ${port}` + port)
+  console.log(`Serving meal-folio API on port: ${port}`)
 })
